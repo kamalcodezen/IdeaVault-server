178958868f23db48ea8dce34a15161427ebb9eb6
kamalcodezen <kamaluddin7908@gmail.com>
2026-06-06 00:20:16 +0530

    database create and collection name added

--- index.js
+++ index.js
@@ -26,9 +26,12 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-
+        const db = client.db("designVault")
+        const designCollection = db.collection("idea")
 
         
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
